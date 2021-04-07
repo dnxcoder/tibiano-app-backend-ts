29fe202d9e5fb29cf2113c1427ac7debc53e01ca
DnxCoder <dnxcoder@gmail.com>
2021-04-07 15:57:34 -0300

    Javascript files compiled with TSC

---
+++ dist/controllers/charBazaarController.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    searchCharBaazar: function (req, res) {
+        res.send('procurando char no bazaar');
+    }
+};
--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = __importDefault(require("./routes"));
 var app = express_1.default();
-app.get('/', function (req, res) {
-    res.send('Este é um test typescript');
-});
-app.listen(8080);
+app.use(routes_1.default);
+var PORT = process.env.PORT || 8877;
+app.listen(PORT);
