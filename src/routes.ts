15f7b90fc9b6b6b9b787120a1b15dfca4f263426
DnxCoder <dnxcoder@gmail.com>
2021-04-07 15:43:06 -0300

    Configuração de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ const router = Router();
 import charBazaarControler from './controllers/charBazaarController';
 
 
-
+router.get('/', (req, res) => { res.send('primeira pagina') })
 
 router.get('/paginaTest', charBazaarControler.searchCharBaazar);
 
