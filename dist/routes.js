25e65afea3ff854060753e010c002aca9ef6c7d9
DnxCoder <dnxcoder@gmail.com>
2021-04-08 22:01:18 -0300

    Test of Deploy

--- dist/routes.js
+++ dist/routes.js
@@ -6,5 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = express_1.Router();
 var charBazaarController_1 = __importDefault(require("./controllers/charBazaarController"));
+router.get('/', function (req, res) { res.send('Welcome to Tibiano App Backend...'); });
 router.post('/searchCharBaazar', charBazaarController_1.default.searchCharBaazar);
 exports.default = router;
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ const router = Router();
 import charBazaarControler from './controllers/charBazaarController';
 
 
-
+router.get('/', (req,res)=>{ res.send('Welcome to Tibiano App Backend...')})
 router.post('/searchCharBaazar', charBazaarControler.searchCharBaazar);
 
 
