5a5dcb4366701e78b467b99c1197b588996f2c40
DnxCoder <dnxcoder@gmail.com>
2021-04-08 22:07:36 -0300

    Added try catch to controller charBazaar

--- dist/controllers/charBazaarController.js
+++ dist/controllers/charBazaarController.js
@@ -47,95 +47,100 @@ exports.default = {
     searchCharBaazar: function (req, res) {
         var _a = req.body, world = _a.world, vocation = _a.vocation, skill = _a.skill, minskill = _a.minskill, maxskill = _a.maxskill, minlevel = _a.minlevel, maxlevel = _a.maxlevel;
         var _b = convertToSelectValues_1.default(world, vocation, skill), vocationCode = _b.vocationCode, skillCode = _b.skillCode;
-        (function () { return __awaiter(void 0, void 0, void 0, function () {
-            var browser, page, characterInformation;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, puppeteer_1.default.launch()];
-                    case 1:
-                        browser = _a.sent();
-                        return [4 /*yield*/, browser.newPage()];
-                    case 2:
-                        page = _a.sent();
-                        return [4 /*yield*/, page.goto('https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades')];
-                    case 3:
-                        _a.sent();
-                        if (!world) return [3 /*break*/, 5];
-                        return [4 /*yield*/, page.select('select[name="filter_world"]', world)];
-                    case 4:
-                        _a.sent();
-                        _a.label = 5;
-                    case 5:
-                        if (!vocation) return [3 /*break*/, 7];
-                        return [4 /*yield*/, page.select('select[name="filter_profession"]', vocationCode)];
-                    case 6:
-                        _a.sent();
-                        _a.label = 7;
-                    case 7:
-                        if (!skill) return [3 /*break*/, 9];
-                        return [4 /*yield*/, page.select('select[name="filter_skillid"]', skillCode)];
-                    case 8:
-                        _a.sent();
-                        _a.label = 9;
-                    case 9:
-                        if (!minlevel) return [3 /*break*/, 11];
-                        return [4 /*yield*/, page.type('input[name="filter_levelrangefrom"]', minlevel)];
-                    case 10:
-                        _a.sent();
-                        _a.label = 11;
-                    case 11:
-                        if (!maxlevel) return [3 /*break*/, 13];
-                        return [4 /*yield*/, page.type('input[name="filter_levelrangeto"]', maxlevel)];
-                    case 12:
-                        _a.sent();
-                        _a.label = 13;
-                    case 13:
-                        if (!minlevel) return [3 /*break*/, 15];
-                        return [4 /*yield*/, page.type('input[name="filter_skillrangefrom"]', minskill)];
-                    case 14:
-                        _a.sent();
-                        _a.label = 15;
-                    case 15:
-                        if (!maxskill) return [3 /*break*/, 17];
-                        return [4 /*yield*/, page.type('input[name="filter_skillrangeto"]', maxskill)];
-                    case 16:
-                        _a.sent();
-                        _a.label = 17;
-                    case 17: return [4 /*yield*/, page.click('input[value="Apply"]')];
-                    case 18:
-                        _a.sent();
-                        return [4 /*yield*/, page.waitForSelector('.Auction')];
-                    case 19:
-                        _a.sent();
-                        return [4 /*yield*/, page.evaluate(function () {
-                                var auctionContainer = document.querySelectorAll('.Auction');
-                                var arrayAuction = Array.from(auctionContainer);
-                                var characterInformation = arrayAuction.map(function (item) {
-                                    var linkImg = item.querySelector('.AuctionOutfitImage').src;
-                                    var name = item.querySelector('.AuctionCharacterName a').text;
-                                    var stringInformation = item.querySelector('.AuctionHeader').innerText;
-                                    stringInformation = stringInformation.replace(/\n/gi, '');
-                                    stringInformation = stringInformation.replace("" + name, '');
-                                    stringInformation = stringInformation.replace('Level: ', '');
-                                    stringInformation = stringInformation.replace('Vocation: ', '');
-                                    stringInformation = stringInformation.replace('World: ', '');
-                                    var arrayInfomation = stringInformation.split('|');
-                                    return ({
-                                        name: arrayInfomation[0],
-                                        vocation: arrayInfomation[1],
-                                        gender: arrayInfomation[2],
-                                        world: arrayInfomation[3],
-                                        img: linkImg
+        try {
+            (function () { return __awaiter(void 0, void 0, void 0, function () {
+                var browser, page, characterInformation;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, puppeteer_1.default.launch()];
+                        case 1:
+                            browser = _a.sent();
+                            return [4 /*yield*/, browser.newPage()];
+                        case 2:
+                            page = _a.sent();
+                            return [4 /*yield*/, page.goto('https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades')];
+                        case 3:
+                            _a.sent();
+                            if (!world) return [3 /*break*/, 5];
+                            return [4 /*yield*/, page.select('select[name="filter_world"]', world)];
+                        case 4:
+                            _a.sent();
+                            _a.label = 5;
+                        case 5:
+                            if (!vocation) return [3 /*break*/, 7];
+                            return [4 /*yield*/, page.select('select[name="filter_profession"]', vocationCode)];
+                        case 6:
+                            _a.sent();
+                            _a.label = 7;
+                        case 7:
+                            if (!skill) return [3 /*break*/, 9];
+                            return [4 /*yield*/, page.select('select[name="filter_skillid"]', skillCode)];
+                        case 8:
+                            _a.sent();
+                            _a.label = 9;
+                        case 9:
+                            if (!minlevel) return [3 /*break*/, 11];
+                            return [4 /*yield*/, page.type('input[name="filter_levelrangefrom"]', minlevel)];
+                        case 10:
+                            _a.sent();
+                            _a.label = 11;
+                        case 11:
+                            if (!maxlevel) return [3 /*break*/, 13];
+                            return [4 /*yield*/, page.type('input[name="filter_levelrangeto"]', maxlevel)];
+                        case 12:
+                            _a.sent();
+                            _a.label = 13;
+                        case 13:
+                            if (!minlevel) return [3 /*break*/, 15];
+                            return [4 /*yield*/, page.type('input[name="filter_skillrangefrom"]', minskill)];
+                        case 14:
+                            _a.sent();
+                            _a.label = 15;
+                        case 15:
+                            if (!maxskill) return [3 /*break*/, 17];
+                            return [4 /*yield*/, page.type('input[name="filter_skillrangeto"]', maxskill)];
+                        case 16:
+                            _a.sent();
+                            _a.label = 17;
+                        case 17: return [4 /*yield*/, page.click('input[value="Apply"]')];
+                        case 18:
+                            _a.sent();
+                            return [4 /*yield*/, page.waitForSelector('.Auction')];
+                        case 19:
+                            _a.sent();
+                            return [4 /*yield*/, page.evaluate(function () {
+                                    var auctionContainer = document.querySelectorAll('.Auction');
+                                    var arrayAuction = Array.from(auctionContainer);
+                                    var characterInformation = arrayAuction.map(function (item) {
+                                        var linkImg = item.querySelector('.AuctionOutfitImage').src;
+                                        var name = item.querySelector('.AuctionCharacterName a').text;
+                                        var stringInformation = item.querySelector('.AuctionHeader').innerText;
+                                        stringInformation = stringInformation.replace(/\n/gi, '');
+                                        stringInformation = stringInformation.replace("" + name, '');
+                                        stringInformation = stringInformation.replace('Level: ', '');
+                                        stringInformation = stringInformation.replace('Vocation: ', '');
+                                        stringInformation = stringInformation.replace('World: ', '');
+                                        var arrayInfomation = stringInformation.split('|');
+                                        return ({
+                                            name: arrayInfomation[0],
+                                            vocation: arrayInfomation[1],
+                                            gender: arrayInfomation[2],
+                                            world: arrayInfomation[3],
+                                            img: linkImg
+                                        });
                                     });
-                                });
-                                return characterInformation;
-                            })];
-                    case 20:
-                        characterInformation = _a.sent();
-                        res.send(JSON.stringify(characterInformation));
-                        return [2 /*return*/];
-                }
-            });
-        }); })();
+                                    return characterInformation;
+                                })];
+                        case 20:
+                            characterInformation = _a.sent();
+                            res.send(JSON.stringify(characterInformation));
+                            return [2 /*return*/];
+                    }
+                });
+            }); })();
+        }
+        catch (e) {
+            res.send(JSON.stringify(e));
+        }
     }
 };
