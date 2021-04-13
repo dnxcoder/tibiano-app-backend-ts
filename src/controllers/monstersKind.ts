8ff95046ec1e7616fcf3b67941c443678c535d66
DnxCoder <dnxcoder@gmail.com>
2021-04-13 17:52:56 -0300

    new route getMonster with puppeteer

--- src/controllers/monstersKind.ts
+++ src/controllers/monstersKind.ts
@@ -3,6 +3,8 @@ import got from 'got';
 import jsdom from 'jsdom';
 import axios from 'axios';
 
+import puppeteer from 'puppeteer';
+
 
 const { JSDOM } = jsdom;
 
@@ -115,8 +117,95 @@ export default {
 
         const { monsterName } = req.query;
 
-        res.send(monsterName)
-        //res.json(`Aqui esta o nome do monstro: ${monsterName}`)
+        (async () => {
+
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ["--no-sandbox"]
+            });
+            const page = await browser.newPage();
+            await page.goto(`https://www.tibiawiki.com.br/wiki/${monsterName}`);
+
+            const monsterInformation: any = await page.evaluate(() => {
+
+                const tableMonsters = document.querySelector('.sortable');
+                const linesFromTableMonster = tableMonsters?.querySelectorAll('tr');
+                var charactersInfoArray: any;
+
+
+                if (linesFromTableMonster) charactersInfoArray = Array.from(linesFromTableMonster).map((line) => {
+
+
+                    let linkImgFiltered: string = '';
+                    let nameMonsterFiltered: string = '';
+                    let hpFiltered: string = '';
+                    let xpFiltered: string = '';
+                    let charmFiltered: string = '';
+
+
+                    var monsterName = line.querySelector('td:nth-child(1) a');
+                    if (monsterName) nameMonsterFiltered = monsterName?.innerHTML;
+
+                    var linkImg = line.querySelector('td:nth-child(2)');
+                    if (linkImg) linkImg = linkImg?.querySelector('img');
+                    if (linkImg) linkImgFiltered = `https://tibiawiki.com.br/${linkImg.getAttribute('src')}`;
+
+
+                    var hpColumn = line.querySelector('td:nth-child(3) ');
+                    var aFromhpColumn = line.querySelector('td:nth-child(3) a');
+
+                    if (aFromhpColumn) {
+                        if (hpColumn) {
+                            hpColumn.removeChild(aFromhpColumn);
+                            hpFiltered = hpColumn.innerHTML;
+                            hpFiltered = hpColumn.innerHTML.replace(/\n/g, '');
+                            hpFiltered = hpFiltered.replace(' ', '');
+                        }
+                    }
+
+                    var xpColumn = line.querySelector('td:nth-child(4) ');
+                    var aFromXpColumn = line.querySelector('td:nth-child(4) a');
+
+                    if (aFromXpColumn) {
+                        if (xpColumn) {
+                            xpColumn.removeChild(aFromXpColumn);
+                            xpFiltered = xpColumn.innerHTML;
+                            xpFiltered = xpFiltered.replace(/\n/g, ' ');
+                            xpFiltered = xpFiltered.replace(/ /g, '');
+                        }
+                    }
+
+                    var charmColumn = line.querySelector('td:nth-child(5)');
+                    var aCharmColum = line.querySelector('td:nth-child(5) a');
+
+                    if (aCharmColum) {
+                        if (charmColumn) {
+                            charmColumn.removeChild(aCharmColum);
+                            charmFiltered = charmColumn.innerHTML;
+                            charmFiltered = charmFiltered.replace(/\n/g, '');
+                            charmFiltered = charmFiltered.replace(/ /g, '');
+                        }
+                    }
+
+                    return (
+                        {
+                            name: nameMonsterFiltered,
+                            hp: hpFiltered,
+                            xp: xpFiltered,
+                            charm: charmFiltered,
+                            link: linkImgFiltered
+                        }
+                    )
+
+                });
+
+                charactersInfoArray.shift();
+                return charactersInfoArray;
+            });
+
+            res.send(monsterInformation);
+
+        })()
 
 
     },
