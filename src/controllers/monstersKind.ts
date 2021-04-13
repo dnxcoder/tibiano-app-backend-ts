b84f55f8beba55ffc9e2785131cad1b9645e7583
DnxCoder <dnxcoder@gmail.com>
2021-04-13 14:42:41 -0300

    added Catch error to tesPostMonster

--- src/controllers/monstersKind.ts
+++ src/controllers/monstersKind.ts
@@ -120,7 +120,6 @@ export default {
 
     },
 
-
     testePostMonsters: (req: Request, res: Response) => {
 
         const { monsterName } = req.body;
@@ -129,88 +128,90 @@ export default {
         const monsterKindURL = `https://www.tibiawiki.com.br/wiki/${monsterName}`
 
 
-        try {
-            got(monsterKindURL)
-                .then(response => {
 
-                    const allDomPage = new JSDOM(response.body.toString()).window.document;
-                    const tableMonsters = allDomPage.querySelector('.sortable');
-                    const linesFromTableMonster = tableMonsters?.querySelectorAll('tr');
-                    var charactersInfoArray: any;
+        got(monsterKindURL)
+            .then(response => {
 
-
-                    if (linesFromTableMonster) charactersInfoArray = Array.from(linesFromTableMonster).map((line) => {
+                const allDomPage = new JSDOM(response.body.toString()).window.document;
+                const tableMonsters = allDomPage.querySelector('.sortable');
+                const linesFromTableMonster = tableMonsters?.querySelectorAll('tr');
+                var charactersInfoArray: any;
 
 
-                        let linkImgFiltered: string = '';
-                        let nameMonsterFiltered: string = '';
-                        let hpFiltered: string = '';
-                        let xpFiltered: string = '';
-                        let charmFiltered: string = '';
+                if (linesFromTableMonster) charactersInfoArray = Array.from(linesFromTableMonster).map((line) => {
 
 
-                        var monsterName = line.querySelector('td:nth-child(1) a');
-                        if (monsterName) nameMonsterFiltered = monsterName?.innerHTML;
+                    let linkImgFiltered: string = '';
+                    let nameMonsterFiltered: string = '';
+                    let hpFiltered: string = '';
+                    let xpFiltered: string = '';
+                    let charmFiltered: string = '';
 
-                        var linkImg = line.querySelector('td:nth-child(2)');
-                        if (linkImg) linkImg = linkImg?.querySelector('img');
-                        if (linkImg) linkImgFiltered = `https://tibiawiki.com.br/${linkImg.getAttribute('src')}`;
+
+                    var monsterName = line.querySelector('td:nth-child(1) a');
+                    if (monsterName) nameMonsterFiltered = monsterName?.innerHTML;
+
+                    var linkImg = line.querySelector('td:nth-child(2)');
+                    if (linkImg) linkImg = linkImg?.querySelector('img');
+                    if (linkImg) linkImgFiltered = `https://tibiawiki.com.br/${linkImg.getAttribute('src')}`;
 
 
-                        var hpColumn = line.querySelector('td:nth-child(3) ');
-                        var aFromhpColumn = line.querySelector('td:nth-child(3) a');
+                    var hpColumn = line.querySelector('td:nth-child(3) ');
+                    var aFromhpColumn = line.querySelector('td:nth-child(3) a');
 
-                        if (aFromhpColumn) {
-                            if (hpColumn) {
-                                hpColumn.removeChild(aFromhpColumn);
-                                hpFiltered = hpColumn.innerHTML;
-                                hpFiltered = hpColumn.innerHTML.replace(/\n/g, '');
-                                hpFiltered = hpFiltered.replace(' ', '');
-                            }
+                    if (aFromhpColumn) {
+                        if (hpColumn) {
+                            hpColumn.removeChild(aFromhpColumn);
+                            hpFiltered = hpColumn.innerHTML;
+                            hpFiltered = hpColumn.innerHTML.replace(/\n/g, '');
+                            hpFiltered = hpFiltered.replace(' ', '');
                         }
+                    }
 
-                        var xpColumn = line.querySelector('td:nth-child(4) ');
-                        var aFromXpColumn = line.querySelector('td:nth-child(4) a');
+                    var xpColumn = line.querySelector('td:nth-child(4) ');
+                    var aFromXpColumn = line.querySelector('td:nth-child(4) a');
 
-                        if (aFromXpColumn) {
-                            if (xpColumn) {
-                                xpColumn.removeChild(aFromXpColumn);
-                                xpFiltered = xpColumn.innerHTML;
-                                xpFiltered = xpFiltered.replace(/\n/g, ' ');
-                                xpFiltered = xpFiltered.replace(/ /g, '');
-                            }
+                    if (aFromXpColumn) {
+                        if (xpColumn) {
+                            xpColumn.removeChild(aFromXpColumn);
+                            xpFiltered = xpColumn.innerHTML;
+                            xpFiltered = xpFiltered.replace(/\n/g, ' ');
+                            xpFiltered = xpFiltered.replace(/ /g, '');
                         }
+                    }
 
-                        var charmColumn = line.querySelector('td:nth-child(5)');
-                        var aCharmColum = line.querySelector('td:nth-child(5) a');
+                    var charmColumn = line.querySelector('td:nth-child(5)');
+                    var aCharmColum = line.querySelector('td:nth-child(5) a');
 
-                        if (aCharmColum) {
-                            if (charmColumn) {
-                                charmColumn.removeChild(aCharmColum);
-                                charmFiltered = charmColumn.innerHTML;
-                                charmFiltered = charmFiltered.replace(/\n/g, '');
-                                charmFiltered = charmFiltered.replace(/ /g, '');
-                            }
+                    if (aCharmColum) {
+                        if (charmColumn) {
+                            charmColumn.removeChild(aCharmColum);
+                            charmFiltered = charmColumn.innerHTML;
+                            charmFiltered = charmFiltered.replace(/\n/g, '');
+                            charmFiltered = charmFiltered.replace(/ /g, '');
                         }
+                    }
 
-                        return (
-                            {
-                                name: nameMonsterFiltered,
-                                hp: hpFiltered,
-                                xp: xpFiltered,
-                                charm: charmFiltered,
-                                link: linkImgFiltered
-                            }
-                        )
+                    return (
+                        {
+                            name: nameMonsterFiltered,
+                            hp: hpFiltered,
+                            xp: xpFiltered,
+                            charm: charmFiltered,
+                            link: linkImgFiltered
+                        }
+                    )
 
-                    });
-
-                    charactersInfoArray.shift();
-                    res.json(charactersInfoArray);
                 });
-        } catch (error) {
-            console.log('Aconteceu este erro aqui: ' + error);
-        }
+
+                charactersInfoArray.shift();
+                res.json(charactersInfoArray);
+            }).catch((error) => {
+
+                res.send('Este é o erro encontrado : ' + error);
+
+            })
+
     },
 
 
